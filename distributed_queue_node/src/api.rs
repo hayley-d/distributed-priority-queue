d18b5ec4a76d2ddc92ee8e0507b7e441132692d2
hayley-d <hayleydod@proton.me>
2024-12-27 09:20:54 +0200

    adding update route

--- distributed_queue_node/src/api.rs
+++ distributed_queue_node/src/api.rs
@@ -1,14 +1,52 @@
-use std::sync::Arc;
-
+use crate::error::ApiError;
+use crate::min_heap::{HeapNode, MinHeap};
 use rocket::serde::json::Json;
 use rocket::{get, post};
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio_postgres::Client;
 
-use crate::error::ApiError;
-use crate::min_heap::{HeapNode, MinHeap};
+/// Contains the API routes for the [distributed_queue_node] in the [distributed priority queue].
+///
+/// The node contains the following API routes:
+///
+/// GET /dequeue
+/// Used to pop the job with the highest priority from the priority queue.
+///
+/// Example Response
+/// ```json
+/// {
+///     "job_id" : 123,
+///     "priority" : 1,
+///     "payload" : [1,1,1,1]
+/// }
+/// ```
+/// GET /dequeue/<amount>
+/// Used to pop <amount> jobs from the priority queue.
+///
+/// Example Response
+/// ```json
+/// {
+///     [
+///         {"job_id":123,"priority":1,"payload":[]},
+///         {"job_id":124,"priority":2,"payload":[]}   
+///     ]
+/// }
+/// ```
+/// POST /enqueue
+/// Enqueues a job to the priority queue.
+///
+/// Example Request:
+/// ```json
+/// {
+///     "priority" : 1,
+///     "payload" : [],
+/// }
+/// ```
+///
 
+/// DequeueResponse represents the response sent by the node when the /dequeue route is used.
 #[derive(Debug, Deserialize, Serialize)]
 pub struct DequeueResponse {
     job_id: i64,
@@ -16,19 +54,34 @@ pub struct DequeueResponse {
     payload: Vec<u8>,
 }
 
+/// BatchDequeueResponse represents the response sent by the node when the /dequeue/<amount> route
+/// is used.
 #[derive(Debug, Deserialize, Serialize)]
 pub struct BatchDequeueResponse {
     jobs: Vec<DequeueResponse>,
 }
 
+/// EnqueueRequest represents the request expected when the /enqueue route is used.
 #[derive(Debug, Deserialize, Serialize)]
 pub struct EnqueueRequest {
     priority: i32,
     payload: Vec<u8>,
 }
 
+/// CreationResponse is the response sent by the node when a job is successfully added into the
+/// priortiy queue.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CreationResponse {
+    message: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UpdateRequest {
+    priority: i32,
+    job_id: i64,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UpdateResponse {
     message: String,
 }
 
@@ -134,4 +187,14 @@ pub async fn enqueue(
     return Ok(Json(CreationResponse {
         message: format!("Job successully added to database"),
     }));
+}
+
+#[post("/update/<job_id>", format = "json", data = "<request>")]
+pub async fn update(
+    job_id: usize,
+    request: Json<UpdateRequest>,
+    db: &rocket::State<Arc<Mutex<Client>>>,
+    heap: &rocket::State<Arc<Mutex<MinHeap>>>,
+) -> Result<UpdateResponse, ApiError> {
+    todo!()
 }
