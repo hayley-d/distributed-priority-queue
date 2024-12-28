f1625f432598dc7e3dc5ce5ea58e5bbcf0cd3368
hayley-d <hayleydod@proton.me>
2024-12-28 18:26:33 +0200

    leader paxos

--- leader/src/grpc.rs
+++ leader/src/grpc.rs
@@ -1,13 +1,13 @@
 use crate::job_management::job_service_server::JobService;
-use crate::job_management::{EnqueueRequest, Job, JobRequest, JobResponse, PaxosPrepare};
+use crate::job_management::paxos_service_client::PaxosServiceClient;
+use crate::job_management::{
+    EnqueueRequest, Job, JobRequest, JobResponse, PaxosCommit, PaxosPrepare, PaxosPropose,
+};
 use crate::node_state::NodeState;
+use log::error;
 use std::sync::Arc;
 use tokio::sync::Mutex;
-use tokio_postgres::Client;
 use tonic::{transport::Server, Request, Response, Status};
-pub mod proto {
-    tonic::include_proto!("job_management");
-}
 
 pub struct LocalJobService {
     node_state: Arc<Mutex<NodeState>>,
@@ -29,24 +29,107 @@ impl JobService for LocalJobService {
         let enqueue_request = request.into_inner();
         let priority = enqueue_request.priority;
         let payload = enqueue_request.payload;
-
-        let paxos_prepare = PaxosPrepare {
-            proposal_number: self.node_state.lock().await.increment_timestamp(),
-        };
+        let proposal_number: i32 = self.node_state.lock().await.increment_timestamp();
+        let mut job_id: i64 = -1;
+        let paxos_prepare = PaxosPrepare { proposal_number };
 
         let mut responses = Vec::new();
 
-        for follower in self.node_state.lock().await.followers {
-            let mut client = PaxosClient::connect(follower).await.unwrap();
+        for follower in &self.node_state.lock().await.followers {
+            let mut client = PaxosServiceClient::connect(follower).await.unwrap();
+            let response = client.prepare(paxos_prepare.clone()).await;
+            responses.push(response);
         }
 
-        Ok(Response::new(JobResponse {
-            job: Some(Job {
+        // Check the first successful PaxosPromise response
+        let mut paxos_promise = None;
+        for response in responses {
+            if let Ok(promise) = response {
+                if promise.get_ref().accepted_value == 1 {
+                    paxos_promise = Some(promise);
+                    break;
+                }
+            }
+        }
+
+        if paxos_promise.is_none() {
+            error!(
+                "Paxos prepared failed with proposal number = {}",
+                proposal_number
+            );
+            return Err(Status::internal("Paxos prepared failed"));
+        }
+
+        job_id = self
+            .node_state
+            .lock()
+            .await
+            .insert_job(priority as u32, payload)
+            .await? as i64;
+
+        let paxos_propose = PaxosPropose {
+            proposal_number,
+            proposed_job: Some(Job {
                 job_id,
                 priority,
-                payload,
+                payload: payload.clone(),
             }),
-        }))
+        };
+
+        let mut responses = Vec::new();
+        for follower in &self.node_state.lock().await.followers {
+            let mut client = PaxosServiceClient::connect(follower).await.unwrap();
+            let response = client.propose(paxos_propose.clone()).await;
+            responses.push(response);
+        }
+
+        let mut paxos_propose = None;
+        for response in responses {
+            if let Ok(propose) = response {
+                if propose.get_ref().proposal_number == proposal_number {
+                    paxos_propose = Some(propose);
+                    break;
+                }
+            }
+        }
+
+        if paxos_propose.is_none() {
+            error!(
+                "Paxos propose failed with proposal number = {}",
+                proposal_number
+            );
+            return Err(Status::internal("Paxos propose failed"));
+        }
+
+        let paxos_commit = PaxosCommit {
+            proposal_number,
+            commit: true,
+        };
+
+        let mut responses = Vec::new();
+        for follower in self.node_state.lock().await.followers {
+            let mut client = PaxosServiceClient::connect(follower).await.unwrap();
+            let response = client.commit(paxos_commit.clone()).await;
+            responses.push(response);
+        }
+
+        for response in responses {
+            if let Ok(_) = response {
+                return Ok(Response::new(JobResponse {
+                    job: Some(Job {
+                        job_id: job_id as i64,
+                        priority,
+                        payload,
+                    }),
+                }));
+            }
+        }
+
+        error!(
+            "Paxos commit failed with proposal_number = {}",
+            proposal_number
+        );
+        return Err(Status::internal("Paxos commit failed"));
     }
 
     async fn get_task(
