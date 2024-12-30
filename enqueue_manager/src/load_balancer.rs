323552f86c56671e98db3adf5c51add55340fe88
hayley-d <hayleydod@proton.me>
2024-12-30 04:00:43 +0200

    setup buffer impl

--- enqueue_manager/src/load_balancer.rs
+++ enqueue_manager/src/load_balancer.rs
@@ -1,1 +1,7 @@
-mod load_balancer {}
+mod load_balancer {
+    const WEIGHT_1: f32 = 0.3;
+    const WIEGHT_2: f32 = 0.25;
+    const WEIGHT_3: f32 = 0.20;
+    const WEIGHT_4: f32 = 0.15;
+    const WEIGHT_5: f32 = 0.10;
+}
